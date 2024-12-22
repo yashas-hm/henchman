b9aa5733ba4c637810039a3d6922f39e5616078e
Yashas H Majmudar <yashashm.dev@gmail.com>
2024-12-22 03:05:29 -0500

    update: main runner

--- henchman.js
+++ henchman.js
@@ -1,7 +1,9 @@
 #! usr/bin/env node
 
-import {initCLI} from "./core/utils.js";
-import {mainMenu} from "./menus/main_menu.js";
+import {initCLI} from './core/utils.js';
+import {mainMenu} from './menus/main_menu.js';
+
+export const baseDir = import.meta.dirname;
 
 if(process.argv.length<=2){
     await mainMenu();

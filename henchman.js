6a0ce4e64167afa25b371b3961f2932dfc680464
Yashas H Majmudar <yashashm.dev@gmail.com>
2024-12-22 00:13:23 -0500

    feat: main runner

--- henchman.js
+++ henchman.js
@@ -1,5 +1,10 @@
 #! usr/bin/env node
 
 import {initCLI} from "./core/utils.js";
+import {mainMenu} from "./menus/main_menu.js";
 
-initCLI();
+if(process.argv.length<=2){
+    await mainMenu();
+}else{
+    initCLI();   
+}

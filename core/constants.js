faac7458a913f6d3f2572a88654c1a0ebf8d35a4
Yashas H Majmudar <yashashm.dev@gmail.com>
2024-12-22 03:03:24 -0500

    update constants

--- core/constants.js
+++ core/constants.js
@@ -1,19 +1,16 @@
-import chalk from "chalk";
+import chalk from 'chalk';
 
 export const henchman = `🧌 ${chalk.green('Henchman')}`;
 export const greetMessage = `${henchman}: Happy to Help!\n`;
-export const byeMessage = `\n${henchman}: Bye, have a great day.`;
-export const invalidChoiceMessage = `\n${henchman}: Invalid choice choose again.`;
-export const errorMessage = `\n${henchman}: Unexpected error occurred, below is the error and stacktrace:\n`;
+export const byeMessage = `${henchman}: Bye, have a great day.`;
+export const errorMessage = `${henchman}: Unexpected error occurred, below is the error and stacktrace:\n`;
 
-export function logo() {
-    console.log(chalk.green(
-        '\n'+
-        '╭╮╱╭╮╱╱╱╱╱╱╱╱╭╮\n' +
-        '┃┃╱┃┃╱╱╱╱╱╱╱╱┃┃\n' +
-        '┃╰━╯┣━━┳━╮╭━━┫╰━┳╮╭┳━━┳━╮\n' +
-        '┃╭━╮┃┃━┫╭╮┫╭━┫╭╮┃╰╯┃╭╮┃╭╮╮\n' +
-        '┃┃╱┃┃┃━┫┃┃┃╰━┫┃┃┃┃┃┃╭╮┃┃┃┃\n' +
-        '╰╯╱╰┻━━┻╯╰┻━━┻╯╰┻┻┻┻╯╰┻╯╰╯\n')
-    );
-}
+export const logo = chalk.green(
+    '\n'+
+    '╭╮╱╭╮╱╱╱╱╱╱╱╱╭╮\n' +
+    '┃┃╱┃┃╱╱╱╱╱╱╱╱┃┃\n' +
+    '┃╰━╯┣━━┳━╮╭━━┫╰━┳╮╭┳━━┳━╮\n' +
+    '┃╭━╮┃┃━┫╭╮┫╭━┫╭╮┃╰╯┃╭╮┃╭╮╮\n' +
+    '┃┃╱┃┃┃━┫┃┃┃╰━┫┃┃┃┃┃┃╭╮┃┃┃┃\n' +
+    '╰╯╱╰┻━━┻╯╰┻━━┻╯╰┻┻┻┻╯╰┻╯╰╯\n'
+)
